042d767fdf7d8d58708c9b8850cd89d2e85355e5
Ken Bateman <novadenizen@gmail.com>
2021-05-12 20:01:22 -0400

    working on primitives

--- modules/hyperbolic.mjs
+++ modules/hyperbolic.mjs
@@ -1,6 +1,19 @@
 
 console.log('hyperbolic.js loaded');
 
+export class Point {
+    constructor(reference, r, theta) {
+        this.reference = reference; 
+        this.r = r;                 // distance from reference
+        this.theta = theta;         // bearing from reference
+        Object.seal(this);
+    }
+    static origin() {
+        return new Point(null, 0, 0);
+    }
+}
+
+
 export function nothing() {
     console.log('hyperbolic.nothing()');
 }
