926b66e3a589152a307a0a7c9d3fa8db394ca11c
Ken Bateman <novadenizen@gmail.com>
2021-05-19 09:55:57 -0400

    Prettier

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,37 +1,36 @@
+import * as hyp from "./oldindex";
+import socket_client from "socket.io-client";
 
-import * as hyp from './oldindex';
-import socket_client from 'socket.io-client';
-
-const URL_SERVER = 'https://www.cymbym.com:58001';
+const URL_SERVER = "https://www.cymbym.com:58001";
 const socket = socket_client(URL_SERVER);
 
 console.log(hyp.nothing());
 const x = new hyp.Complex(2, -3);
 console.log(`x = ${x}`);
 
-
-socket.on('message', (data: any) => { alert(data) });
-socket.on('newdata', (data: any[]) => {
-    const ndDiv = document.getElementById('newdata');
+socket.on("message", (data: any) => {
+    alert(data);
+});
+socket.on("newdata", (data: any[]) => {
+    const ndDiv = document.getElementById("newdata");
     if (ndDiv) {
-        ndDiv.textContent = '';
+        ndDiv.textContent = "";
         for (const row of data) {
-            let line = '';
+            let line = "";
             for (const [key, value] of Object.entries(row)) {
                 line = line + ` ${key}:${value}`;
             }
             const node = document.createTextNode(line);
-            const p = document.createElement('p');
+            const p = document.createElement("p");
             p.appendChild(node);
             ndDiv.appendChild(p);
         }
     }
 });
 
-const newdatabutton = document.getElementById('newdatabutton');
+const newdatabutton = document.getElementById("newdatabutton");
 if (newdatabutton) {
     newdatabutton.onclick = () => {
-        socket.emit('getnewdata');
+        socket.emit("getnewdata");
     };
 }
-
