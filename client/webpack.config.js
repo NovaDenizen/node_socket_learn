7b17b88a0942a21dd4fa66e624a8047b7862a9cf
Ken Bateman <novadenizen@gmail.com>
2021-06-25 16:44:33 -0400

    changed from production to development mode.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,6 +19,6 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
     },
     devtool: "source-map",
-    //mode: "development",
-    mode: 'production'
+    mode: "development",
+    //mode: 'production'
 };
