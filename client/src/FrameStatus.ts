b306f3a64b4dea2e54c1eb7be7c85ed03d1d7972
Ken Bateman <novadenizen@gmail.com>
2021-07-15 15:24:19 -0400

    added FrameStatus.ts

---
+++ client/src/FrameStatus.ts
@@ -0,0 +1,61 @@
+
+
+export default class FrameStatus {
+    private _statusPeriodMs: number = 10000;
+    private frameDurations: number[] = [];
+    private frameStart: number | undefined = undefined;
+    logger: ((s: string) => void) | undefined;
+    interval: number | undefined;
+    startFrame(): void {
+        if (this.frameStart !== undefined) {
+            throw new Error("this.frameStart !== undefined in startFrame");
+        }
+        this.frameStart = new Date().getTime();
+    }
+    endFrame(): void {
+        if (this.frameStart === undefined) {
+            throw new Error("this.frameStart === undefinned in endFrame");
+        }
+        const now = new Date().getTime();
+        const interval = now - this.frameStart;
+        this.frameDurations.push(interval);
+    }
+    private clearInterval(): void {
+        if (this.interval !== undefined) {
+            window.clearInterval(this.interval);
+            this.interval = undefined;
+        }
+    }
+    private initInterval(): void {
+        this.interval = window.setInterval(this.sendStatus.bind(this), this._statusPeriodMs);
+    }
+    set statusPeriodMs(n: number) {
+        this._statusPeriodMs = n;
+        this.sendStatus();
+        this.log(`updated interval to ${n}ms`);
+    }
+    get statusPeriodMs(): number {
+        return this._statusPeriodMs;
+    }
+    log(s: string) {
+        if (this.logger) {
+            this.logger(s);
+        }
+    }
+    constructor() {
+        this.logger = undefined;
+        this.interval = undefined;
+        Object.seal(this);
+        this.initInterval();
+    }
+    private sendStatus() {
+        let sum = 0;
+        for (const d of this.frameDurations) {
+            sum += d;
+        }
+        const nf = this.frameDurations.length;
+        const avg = sum / nf;
+        this.log(`frameStatus: ${nf} frames, avg ${avg}ms}`);
+        this.frameDurations = [];
+    }
+}
